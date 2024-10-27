e228d9ad4b11c77b30bca2888f2fdbba588b0045
upasana-mukherjee <umukherjee.189@gmail.com>
2024-10-27 21:18:23 +1100

    switch between profile & matches

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import {User, MessageCircle, X, Heart } from 'lucide-react';
+import React, { useState } from 'react';
+
 const ProfileSelector = () => (
     <div className= "rounded-lg overflow-hidden bg-white shadow-lg">
       <div className= "relative">
@@ -46,14 +48,22 @@ const MatchesList = () => (
   </div>
 )
 function App() {
+  const [currentScreen, setCurrentScreen] = useState('profile');
+  const renderScreen = () => {
+    switch (currentScreen) {
+      case 'profile':
+          return <ProfileSelector />;
+      case 'matches':
+          return <MatchesList />;
+    }
+  }
   return (
   <div className="max-w-md mx-auto p-4">
     <nav className="flex justify-between mb-4">
-      <User />
-      <MessageCircle />
+      <User onClick={() => setCurrentScreen("profile")} />
+      <MessageCircle onClick={() => setCurrentScreen("matches")} />
     </nav>
-    <ProfileSelector />
-    {/* <MatchesList /> */}
+    {renderScreen()}
   </div>
   )
 }
